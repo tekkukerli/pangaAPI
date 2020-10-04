8c5435f504abec95ca9dd2666c6c6c9b63828343
tekkukerli <kerlitekku@gmail.com>
2020-10-04 14:02:27 +0300

    wip

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express');
+const yaml = require('yamljs')
+const swaggerDocument = yaml.load('./docs/swagger.yaml');
+const port = "8080"
+
+//Copy env variables to pocess.env
+require('dotenv').config()
+
+const app = express()
+
+//Import routes
+const usersRouter = require('./routes/users')
+const sessionsRouter = require('./routes/sessions')
+
+//Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,  //kas on vaja?
+    useUnifiedTopology: true
+}, ()=>{
+    console.log('Connected to DB!')
+})
+
+//Run middlewares
+app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//Attatch routes
+app.use(usersRouter)
+app.use(sessionsRouter)
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
