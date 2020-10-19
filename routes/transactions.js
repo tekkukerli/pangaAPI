55d7c04686823578274bf7b806b9673469bab724
tekkukerli <kerlitekku@gmail.com>
2020-10-19 23:00:58 +0300

    uus commit 3

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,94 +1,88 @@
 const router = require('express').Router()
+const transactionModel = require('../models/Transaction')
 const accountModel = require('../models/Account')
 const bankModel = require('../models/Bank')
 const userModel = require('../models/User')
-const {verifyToken} = require('../middlewares')
 const fetch = require('node-fetch')
-require('dotenv').config()
-const transactionModel = require('../models/Transaction')
+const {verifyToken, refreshBanksFromCentralBank} = require('../middlewares')
 
-//Log in 
-router.post('/',verifyToken, async(req, res, next) => {
+require('dotenv').config()
+
+router.post('/', verifyToken, async(req, res, next) => {
+
+    let banks = [], statusDetail
 
     //Get account data from DB
     const accountFromObject = await accountModel.findOne({number: req.body.accountFrom})
 
     //Check that account exists
-    if(!accountFromObject){
+    if (!accountFromObject){
         return res.status(404).json({error: 'Account not found'})
     }
 
     //Check that accountFrom belongs to the user
-    if(accountFromObject.userId.toString() !== req.userId.toString()){
+    if (accountFromObject.userId.toString() !== req.userId.toString()) {
         return res.status(403).json({error: 'Forbidden'})
     }
 
     //Check for sufficient funds
-    if(req.body.amount > accountFromObject.balance){
+    if (req.body.amount > accountFromObject.balance) {
         return res.status(402).json({error: 'Insufficient funds'})
     }
 
     //Check for invalid amounts
-    if(req.body.amount <= 0){
+    if (req.body.amount <= 0) {
         return res.status(400).json({error: 'Invalid amount'})
     }
 
-    const bankToPrefix = req.body.accountTo.slice(0,3)
+    const bankToPrefix = req.body.accountTo.slice(0, 3)
     let bankTo = await bankModel.findOne({bankPrefix: bankToPrefix})
 
     //Check destination bank
     if (!bankTo) {
-        console.log('Destination bank NOT was found in cache')
-        const banks = await fetch(`${process.env.CENTRAL_BANK_URL}/banks`, {
-            headers: {'Api-Key': process.env.CENTRAL_BANK_API_KEY}
-        })
-            .then(responseText => responseText.json())
 
-        //Delete all old banks
-        await bankModel.deleteMany()
+        //Refresh banks from central bank
+        const result = await refreshBanksFromCentralBank();
 
-        //Create new bulk object
-        const bulk = bankModel.collection.initializeUnorderedBulkOp();
+        //Check if there was an error
+        if (typeof result.error !== 'undefined') {
 
-        //Add banks to queue to be inserted into DB
-        banks.forEach(bank => {
-            bulk.insert(bank);
-        })
+            //Log the error to transaction
+            console.log('There was an error communicating with central bank: ')
+            console.log(result.error)
+            statusDetail = result.error
 
-        //Start bulk insert
-        await bulk.execute();
+        } else {
 
-       // console.log(await bankModel.find());
+            //Try getting the details of the destination bank again
+            bankTo = await bankModel.findOne({bankPrefix: bankToPrefix})
 
-        //Try getting the details of the destination bank again
-        bankTo = await bankModel.findOne({bankPrefix: bankToPrefix})
+            //Check for destination bank once more
+            if (!bankTo) {
+                return res.status(400).json ({error: 'Invalid accountTo'})
+            }
 
-        //Check for destination bank once more
-        if(!bankTo){
-            return res.status(400).json ({error: 'Invalid accountTo'})
         }
 
     } else {
         console.log('Destination bank was found in cache')
     }
 
-    //console.log(bankTo);
-
     //Make new transaction
+    console.log("Creating transaction...")
     const transaction = transactionModel.create({
-        userId: req.userId,
-        amount: req.body.amount,
-        currency: accountFromObject.currency,
+        userId:      req.userId,
+        amount:      req.body.amount,
+        currency:    accountFromObject.currency,
         accountFrom: req.body.accountFrom,
-        accountTo: req.body.accountTo,
+        accountTo:   req.body.accountTo,
         explanation: req.body.explanation,
-        senderName: (await userModel.findOne({_id: req.userId})).name
+        statusDetail,
+        senderName:  (await userModel.findOne({_id: req.userId})).name
     })
 
 
-    return res.status(200).json()
+    return res.status(201).json()
 })
-
-
 
 module.exports = router
