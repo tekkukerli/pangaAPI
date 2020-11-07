4df15bd67225557dd74e185ef2fce10e064e8e3e
tekkukerli <kerlitekku@gmail.com>
2020-11-07 18:21:51 +0200

    mocking

--- middlewares.js
+++ middlewares.js
@@ -56,7 +56,6 @@ exports.processTransactions = async () => {
 
     //Get pending transactions
     const pendingTransactions = await transactionModel.find({status: 'pending'})
-
     //Loop through each transaction and send a request
     pendingTransactions.forEach(async transaction => {
 
@@ -147,6 +146,21 @@ exports.processTransactions = async () => {
         //Send request to remote bank
         try {
 
+            const nock = require('nock')
+            let nockScope
+
+            if (process.env.TEST_MODE === 'true') {
+
+                const nockUrl = new URL(bankTo.transactionUrl)
+
+                console.log('Nocking '+ JSON.stringify(nockUrl));
+
+                nockScope = nock(`${nockUrl.protocol}//${nockUrl.host}`)
+                    .persist()
+                    .post(nockUrl.pathname)
+                    .reply(200, {receiverName: 'Krispin'})
+
+            }
             console.log('loop: Making request to ' + bankTo.transactionUrl);
 
             //Abort connection after 1 sec
@@ -247,7 +261,36 @@ exports.processTransactions = async () => {
 exports.refreshBanksFromCentralBank = async () => {
 
     try {
+        let nockScope, nock
+
         console.log('Refreshing banks');
+
+         // Mock central bank responses in TEST_MODE
+         if (process.env.TEST_MODE === 'true') {
+            nock = require('nock')
+            console.log(process.env.TEST_MODE === 'true');
+            nockScope = nock(process.env.CENTRAL_BANK_URL)
+                .persist()
+                .get('/banks')
+                .reply(200,
+                    [
+                        {
+                          "name": "Bank A",
+                          "owners": "John Doe",
+                          "jwksUrl": "https://banka.com/jwks",
+                          "transactionUrl": "https://banka.com/transactions/b2b",
+                          "bankPrefix": "aaa"
+                        },
+                        {
+                          "name": "Bank B",
+                          "owners": "Jane Doe",
+                          "jwksUrl": "https://bankb.com/jwks",
+                          "transactionUrl": "https://bankb.com/transactions/b2b",
+                          "bankPrefix": "bbb"
+                        }
+                      ]
+                )
+        }
 
         console.log('Attempting to contact central bank at ' + `${process.env.CENTRAL_BANK_URL}/banks`)
         let banks = await fetch(`${process.env.CENTRAL_BANK_URL}/banks`, {
@@ -270,6 +313,7 @@ exports.refreshBanksFromCentralBank = async () => {
         await bulk.execute();
 
     } catch (e) {
+        console.log(e.message);
         return {error: e.message}
     }
 
